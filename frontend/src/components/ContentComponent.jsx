7544f7d7dba01ac450c8e9fafe80cc844823067f
Alex <kaplunalex1985@gmail.com>
2023-01-31 08:24:06 +0200

    Refactor to new database config

--- frontend/src/components/ContentComponent.jsx
+++ frontend/src/components/ContentComponent.jsx
@@ -53,28 +53,28 @@ export default function  ContentComponent () {
         <Row>
             <Col className="col-sm-2">
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText(descriptionData[0]?.about_field)} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[0]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[0]?.about_field)} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[7]?.name_of_category}</Button>
                 </div>    
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText(descriptionData[2]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[1]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[1]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[6]?.name_of_category}</Button>
                 </div> 
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText(descriptionData[5]?.about_field)} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[2]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[2]?.about_field)} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[5]?.name_of_category}</Button>
                 </div>    
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText(descriptionData[3]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[3]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[3]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[4]?.name_of_category}</Button>
                 </div> 
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText('img')} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[4]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[4]?.about_field)} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[3]?.name_of_category}</Button>
                 </div>    
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText(descriptionData[1]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[5]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[5]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[2]?.name_of_category}</Button>
                 </div> 
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText(descriptionData[4]?.about_field)} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[6]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[6]?.about_field)} variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[1]?.name_of_category}</Button>
                 </div>    
                 <div className="row align-items-center justify-content-center">
-                    <Button onClick={() => setText(descriptionData[6]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[7]?.name_of_category}</Button>
+                    <Button onClick={() => setText(descriptionData[7]?.about_field)}variant="light" className="btn btn-outline-dark mybtn btn-block vikingfont">{buttonData[0]?.name_of_category}</Button>
                 </div> 
 
             </Col>
